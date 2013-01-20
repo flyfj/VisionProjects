8b44922146a0848a59eb9efd879f83bd372a7052
Jie Feng <flyfengjie@gmail.com>
2013-01-20 14:54:33 -0500

    modified denight program

--- Denight/Denight/work_main.cpp
+++ Denight/Denight/work_main.cpp
@@ -81,33 +81,36 @@ bool ShiftDFT(Mat& src_arr, Mat& dst_arr )
 //	do convolution in Frequency domain
 //	@return		final image after convolution
 //////////////////////////////////////////////////////////////////////////
-bool cvFFTConvolution(const Mat& channel, const Mat& kernel, Mat& conv)
+Mat cvFFTConvolution(const Mat& channel, const Mat& kernel)
 {
 	//create complex image and kernel
 	//////////////////////////////////////////////////////////////////////////
+	Mat realImg = channel.clone();
+	Mat imaginaryImg = channel.clone();
+	imaginaryImg.setTo(0);
 	Mat complexImg(channel.rows, channel.cols, CV_32FC2);
-	vector<Mat> vec_mat(2);
-	vec_mat[0] = channel.clone();
-	vec_mat[1] = channel.clone();
-	vec_mat[1].setTo(0);
+	vector<Mat> vec_mat;
+	vec_mat.push_back( realImg );
+	vec_mat.push_back( imaginaryImg );
 	merge(vec_mat, complexImg);
-
 	//////////////////////////////////////////////////////////////////////////
-	Mat complexKernel(kernel.rows, kernel.cols, CV_32FC2);
-	kernel.copyTo(vec_mat[0]);
-	kernel.copyTo(vec_mat[1]);
-	vec_mat[1].setTo(0);
-	vec_mat[1] = kernel.clone();
+	Mat realKernel = kernel.clone();
+	Mat imaginaryKernel = kernel.clone();
+	imaginaryKernel.setTo(0);
+	Mat complexKernel(realKernel.rows, realKernel.cols, CV_32FC2);
+	vec_mat[0] = realKernel;
+	vec_mat[1] = imaginaryKernel;
 	merge(vec_mat, complexKernel);
 
 	//////////////////////////////////////////////////////////////////////////
-	/*int dft_M = getOptimalDFTSize(channel.rows*2-1);
-	int dft_N = getOptimalDFTSize(channel.cols*2-1);*/
+	int dft_M = getOptimalDFTSize(channel.rows*2-1);
+	int dft_N = getOptimalDFTSize(channel.cols*2-1);
 
 	Mat dft_A(channel.rows, channel.cols, CV_32FC2);
 	dft_A.setTo(0);
 	Mat dft_B(kernel.rows, kernel.cols, CV_32FC2);
 	dft_B.setTo(0);
+	Mat conv = channel.clone();
 
 	//////////////////////////////////////////////////////////////////////////
 	// do dft for image
@@ -139,17 +142,21 @@ bool cvFFTConvolution(const Mat& channel, const Mat& kernel, Mat& conv)
 
 	//do inverse dft
 	dft(dft_A, dft_A, CV_DXT_INV_SCALE, channel.rows);
-
+	Mat realRes(dft_A.rows, dft_A.cols, CV_32F);
+	Mat imageRes = realRes.clone();
+	vec_mat.clear();
 	split(dft_A, vec_mat);
-	pow(vec_mat[0], 2, vec_mat[0]);
-	pow(vec_mat[1], 2, vec_mat[1]);
-	vec_mat[0] += vec_mat[1];
-	pow(vec_mat[0], 2, vec_mat[0]);
+	vec_mat[0].copyTo(realRes);
+	vec_mat[1].copyTo(imageRes);
+	pow(realRes, 2, realRes);
+	pow(imageRes, 2, imageRes);
+	realRes += imageRes;
+	pow(realRes, 0.5, realRes);
 
 	//copy back
-	vec_mat[0].copyTo(conv);
+	realRes.copyTo(conv);
 
-	return true;
+	return conv;
 }
 
 
@@ -166,10 +173,7 @@ int main()
 	namedWindow("res",1);
 
 	//obtain image
-	Mat old_img = imread("dark.jpg");
-	Mat img;
-	resize(old_img, img, Size(old_img.cols/2, old_img.rows/2));
-
+	Mat img = imread("cg_ada.jpg");
 	Mat dimg(img.rows, img.cols, CV_32FC3);
 	img.convertTo(dimg, dimg.depth());
 	//split to 3 channels
@@ -200,10 +204,8 @@ int main()
 			}
 		}
 		sum = 1.0f/sum;
-		
 		//normalize
-		gaussKernels[i] *= sum;
-		//gaussKernels[i].convertTo(gaussKernels[i], gaussKernels[i].depth(), sum);
+		gaussKernels[i].convertTo(gaussKernels[i], gaussKernels[i].depth(), sum);
 	}
 
 	//////////////////////////////////////////////////////////////////////////
@@ -212,7 +214,6 @@ int main()
 	vector<Mat> scales(3);
 	for(int i=0; i<3; i++)
 		scales[i].create(img.rows, img.cols, CV_32F);
-
 	Mat logimg1(img.rows, img.cols, CV_32F);
 	Mat logimg2 = logimg1.clone();
 	
@@ -238,17 +239,20 @@ int main()
 
 		//convert to log
 		//avoid being divided by 0
-		logimg1 = channels[i] + 1.0;
+		logtemp = channels[i] += 1.0;
 		//compute R(i,j)
-		log(logimg1, logimg1);
+		log(logtemp, logimg1);
 
 		//process each scale
 		for(int j=0; j<3; j++)
 		{
-			cvFFTConvolution(channels[i], gaussKernels[j], scales[j]);
-			scales[j] += 1.0;
-			log(scales[j], scales[j]);
-			scales[j] = logimg1 - scales[j];
+			scales[j] = cvFFTConvolution(channels[i], gaussKernels[j]);
+			/*double minv = 0;
+			cvMinMaxLoc(res, &minv, 0, 0, 0, 0);
+			cvSubS(res, cvRealScalar(minv), scales[j]);*/
+			logtemp = scales[j] += 1.0;
+			log(logtemp, logimg2);
+			scales[j] = logimg1 - logimg2;
 		}
 
 		//combine multiscale
@@ -287,21 +291,11 @@ int main()
 	//cvConvertScale(dimg, dimg, 255.0f);
 	Mat result = img.clone();
 	dimg.convertTo(result, result.depth());
+
 	
 	imshow("win", img);
 	imshow("res", result);
-	waitKey(10);
-
-	Mat filterimg = img.clone();
-	bilateralFilter ( result, filterimg, 11, 2*11, 11/2 );
-	//blur(result, result, Size(3,3));
-	imshow("bi", filterimg);
-	waitKey(10);
-	
-	medianBlur(result, filterimg, 3);
-	imshow("Median", filterimg);
-	waitKey(0);
-	
+	cvWaitKey(0);
 
 	cvDestroyAllWindows();
 
