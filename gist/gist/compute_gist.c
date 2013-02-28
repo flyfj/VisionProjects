a704239ec4806e76aa621587317d74d348fa9ec2
Jie Feng <fj333xx@gmail.com>
2013-02-27 22:27:02 -0500

    sf

--- gist/gist/compute_gist.c
+++ gist/gist/compute_gist.c
@@ -56,59 +56,59 @@ static void usage(void) {
 
 
 
-//int main(int argc,char **args) {
-//  
-//  const char *infilename="/dev/stdin";
-//  int nblocks=4;
-//  int n_scale=3;
-//  int orientations_per_scale[50]={8,8,4};
-//  
-//
-//  while(*++args) {
-//    const char *a=*args;
-//    
-//    if(!strcmp(a,"-h")) usage();
-//    else if(!strcmp(a,"-nblocks")) {
-//      if(!sscanf(*++args,"%d",&nblocks)) {
-//        fprintf(stderr,"could not parse %s argument",a); 
-//        usage();
-//      }
-//    } else if(!strcmp(a,"-orientationsPerScale")) {
-//      char *c;
-//      n_scale=0;
-//      for(c=strtok(*++args,",");c;c=strtok(NULL,",")) {
-//        if(!sscanf(c,"%d",&orientations_per_scale[n_scale++])) {
-//          fprintf(stderr,"could not parse %s argument",a); 
-//          usage();         
-//        }
-//      }
-//    } else {
-//      infilename=a;
-//    }
-//  }
-//
-//  color_image_t *im=load_ppm(infilename);
-//  
-//  float *desc = color_gist_scaletab(im,nblocks,n_scale,orientations_per_scale);
-//
-//  int i;
-//  
-//  int descsize=0;
-//  /* compute descriptor size */
-//  for(i=0;i<n_scale;i++) 
-//    descsize+=nblocks*nblocks*orientations_per_scale[i];
-//
-//  descsize*=3; /* color */
-//
-//  /* print descriptor */
-//  for(i=0;i<descsize;i++) 
-//    printf("%.4f ",desc[i]);
-//
-//  printf("\n");
-//  
-//  free(desc);
-//
-//  color_image_delete(im);
-//
-//  return 0; 
-//}
+int main(int argc,char **args) {
+  
+  const char *infilename="/dev/stdin";
+  int nblocks=4;
+  int n_scale=3;
+  int orientations_per_scale[50]={8,8,4};
+  
+
+  while(*++args) {
+    const char *a=*args;
+    
+    if(!strcmp(a,"-h")) usage();
+    else if(!strcmp(a,"-nblocks")) {
+      if(!sscanf(*++args,"%d",&nblocks)) {
+        fprintf(stderr,"could not parse %s argument",a); 
+        usage();
+      }
+    } else if(!strcmp(a,"-orientationsPerScale")) {
+      char *c;
+      n_scale=0;
+      for(c=strtok(*++args,",");c;c=strtok(NULL,",")) {
+        if(!sscanf(c,"%d",&orientations_per_scale[n_scale++])) {
+          fprintf(stderr,"could not parse %s argument",a); 
+          usage();         
+        }
+      }
+    } else {
+      infilename=a;
+    }
+  }
+
+  color_image_t *im=load_ppm(infilename);
+
+  float *desc = color_gist_scaletab(im,nblocks,n_scale,orientations_per_scale);
+
+  int i;
+  
+  int descsize=0;
+  /* compute descriptor size */
+  for(i=0;i<n_scale;i++) 
+    descsize+=nblocks*nblocks*orientations_per_scale[i];
+
+  descsize*=3; /* color */
+
+  /* print descriptor */
+  for(i=0;i<descsize;i++) 
+    printf("%.4f ",desc[i]);
+
+  printf("\n");
+  
+  free(desc);
+
+  color_image_delete(im);
+
+  return 0; 
+}
--- gistdescriptor/batchproc.m
+++ gistdescriptor/batchproc.m
@@ -5,26 +5,58 @@ param.orientationsPerScale = [8 8 8 8]; % number of orientations per scale (from
 param.numberBlocks = 4;
 param.fc_prefilt = 4;
 
-% file directory
-imgdir = 'H:\AmazonProductData\DemoDataSet_resized\Apple_Desktops\';
-gist_resdir = 'H:\MobileVisualSearch\gist\Apple_Desktops\';
-readpath = [imgdir '*.jpg'];
-readlist = dir(readpath);   % save readlist as image files
-[m,n] = size(readlist);
+imgdir = 'F:\Datasets\MobileProductSearch\Whole_Dataset\AmazonProductSet\DemoDataSet_resized\';
+resdir = 'F:\Results\MobileVisualSearch\gist\';
 
-% Pre-allocate gist:
-Nfeatures = sum(param.orientationsPerScale)*param.numberBlocks^2;
-gist = zeros([m Nfeatures]); 
+% category list
+cate_list_file = 'F:\Datasets\MobileProductSearch\Whole_Dataset\AmazonProductSet\test_categories.txt';
+fid = fopen(cate_list_file);
+cate_list = textscan(fid, '%s');
+fclose(fid);
+cate_list = cate_list{1,1};
 
-% Load first image and compute gist:
-img = imread( [imgdir readlist(1,1).name] );
-[gist(1, :), param] = LMgist(img, '', param); % first call
-% Loop:
-for i = 2:m
-   img = imread( [imgdir readlist(i,1).name] );
-   gist(i, :) = LMgist(img, '', param); % the next calls will be faster
-   disp([num2str(i) 'th img done.']);
+for id=1:size(cate_list,1)
+    
+    savefile = [resdir cate_list{id,1} '.mat'];
+    if(exist(savefile, 'file'))
+        continue;
+    end
+    
+    disp(cate_list{id,1});
+    
+    % file directory
+    curdir = [imgdir cate_list{id,1} '\'];
+    gist_resdir = [resdir cate_list{id,1} '\'];
+    mkdir(gist_resdir);
+    readpath = [curdir '*.jpg'];
+    readlist = dir(readpath);   % save readlist as image files
+    [m,n] = size(readlist);
+    
+    if(m==0)
+        continue;
+    end
+
+    % Pre-allocate gist:
+    Nfeatures = sum(param.orientationsPerScale)*param.numberBlocks^2;
+    gist = zeros([m Nfeatures]); 
+
+    % Load first image and compute gist:
+    img = imread( [curdir readlist(1,1).name] );
+    [gist(1, :), param] = LMgist(img, '', param); % first call
+    % Loop:
+    for i = 2:m
+       img = imread( [curdir readlist(i,1).name] );
+       gist(i, :) = LMgist(img, '', param); % the next calls will be faster
+       disp([num2str(i) 'th img done.']);
+    end
+    
+    
+    save(savefile, 'gist');
+    
+    disp(['Save gist to ' savefile]);
+    
 end
+
 
 disp('Finish.');
 
